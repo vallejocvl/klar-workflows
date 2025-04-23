ced23d18d77fc5af09da7fc509d9824cf6d75f09
vallejocvl <vallejo.cvl@me.com>
2025-04-23 15:03:03 -0600

    Added meta_insights_consolidator

---
+++ meta_insights-consolidator/README.md
@@ -0,0 +1,2 @@
+# meta_insights_consolidator
+Consolidates exported csv's from meta to one file. To be used for Think and other's analytics before Looker Studio
---
+++ meta_insights-consolidator/consolidator.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import os
+
+# Set the folder path where your CSVs are stored
+folder_path = 'csv'
+
+# Change this if your join key column has a different name
+join_column = 'name'  
+
+# Get all CSV files in the folder
+csv_files = [file for file in os.listdir(folder_path) if file.endswith('.csv')]
+
+# Iterate through csv's and join them
+merged_df = None
+
+for file in csv_files:
+    df = pd.read_csv(folder_path+'/'+file, encoding='utf-16', skiprows=2)
+    df.columns.values[1] = file.replace('.csv','')
+    
+    if merged_df is None:
+        merged_df = df  # First file sets the base
+    else:
+        merged_df = pd.merge(merged_df, df, on='Date', how='outer')
+
+merged_df.to_csv('merged_df1.csv', index=False)
